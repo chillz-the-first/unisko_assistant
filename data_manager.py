d5f7d342beb3c12508c64528571f82d2146fedd8
chillz-the-first <105022195+chillz-the-first@users.noreply.github.com>
2026-04-28 04:54:53 +0200

    Added logging function
    Added function that logs any question that was not the FAQ sheet

--- data_manager.py
+++ data_manager.py
@@ -2,11 +2,12 @@ import gspread
 from google.oauth2.service_account import Credentials
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 
 load_dotenv()
 
 SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets.readonly",
+    "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.readonly"
 ]
 
@@ -28,5 +29,11 @@ def get_faq():
     return faq_text
 
 
-def log_unanswered_question(parent_number, parent_message):
-    return None
+def log_unanswered_question(parent_number, question):
+    """Logs an unanswered question to the UnansweredQuestions sheet."""
+    client = get_google_client()
+    sheet = client.open_by_key(os.environ["UNANSWERED_SHEET_ID"]).sheet1
+
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    sheet.append_row([timestamp, parent_number, question, "Pending"])
